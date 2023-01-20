79848f23e82bb51d2727dc07e498641207968d99
Hernando Pinzon <hernandonunez13@gmail.com>
2023-01-19 19:16:38 -0500

    Routes

--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,9 @@
 const EXPRESS = require("express");
 const user_schema = require("../models/users");
+const router = EXPRESS.Router();
+
+router.get("/", (req, res) => {});
+router.post("/", (req, res) => {});
+router.get("/:user_id", (req, res) => {});
+router.put("/:user_id", (req, res) => {});
+router.delete("/:user_id", (req, res) => {});
